6009997550e497872a011a4cbb490ffb862f4c87
Ahmadullah Mirza <mirzaahmad9028.am@gmail.com>
2026-03-09 21:08:55 +0530

    feat: remove BrowserRouter wrapper from main rendering

--- src/main.tsx
+++ src/main.tsx
@@ -4,14 +4,11 @@ import { createRoot } from "react-dom/client"
 import "./index.css"
 import App from "./App.tsx"
 import { ThemeProvider } from "@/components/theme-provider.tsx"
-import { BrowserRouter } from "react-router"
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
-      <ThemeProvider>
-        <App />
-      </ThemeProvider>
-    </BrowserRouter>
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
   </StrictMode>
 )
